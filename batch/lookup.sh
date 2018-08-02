beb4055b1aa62350d2e3084db326ef722127eed1
jang <jang_inhwa@microad.co.jp>
2018-08-02 12:56:46 +0900

    Reformat indents sh

--- batch/lookup.sh
+++ batch/lookup.sh
@@ -7,18 +7,18 @@ WAITING_SEC=600
 
 for i in $(seq 1 ${END});
 do
-	if [ -e ./${target} ]; then
-		echo "### flag found. run digdag >>> "
-		digdag run task_transfer.dig
-		rm -rf .digdag/
-		break
-	else
-		if [ ${i} -eq ${END} ]; then
-			echo "### flag not found. send mail ... "
-			cat lookup_mail.txt | sendmail -i -t
-		else
-			echo "### waiting count ... "${i}" /$((${END} - 1))"
-			sleep ${WAITING_SEC}
-		fi
-	fi
+  if [ -e ./${target} ]; then
+    echo "### flag found. run digdag >>> "
+    digdag run task_transfer.dig
+    rm -rf .digdag/
+    break
+  else
+    if [ ${i} -eq ${END} ]; then
+      echo "### flag not found. send mail ... "
+      cat lookup_mail.txt | sendmail -i -t
+    else
+      echo "### waiting count ... "${i}" /$((${END} - 1))"
+      sleep ${WAITING_SEC}
+    fi
+  fi
 done
